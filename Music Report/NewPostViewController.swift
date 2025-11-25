e96298cbf3d3555a0f252bb636fb99d251782a7a
andresnserna <andresnserna@icloud.com>
2025-11-24 19:55:28 -0600

    creating all the VCs and set up the CoreData model

---
+++ Music Report/NewPostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewPostViewController.swift
+//  Music Report
+//
+//  Created by Andrés Serna on 11/24/25.
+//
+
+import UIKit
+
+class NewPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
