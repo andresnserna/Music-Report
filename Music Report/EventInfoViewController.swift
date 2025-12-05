02409fa76353dce29675fbb4963bd31af9a4bded
andresnserna <andresnserna@icloud.com>
2025-12-04 21:19:33 -0600

    working on eventinfoVC

--- Music Report/EventInfoViewController.swift
+++ Music Report/EventInfoViewController.swift
@@ -10,12 +10,13 @@ import MapKit
 
 class EventInfoViewController: UIViewController {
     
-    var selectedEvent: Event?
+    var selectedEvent: Events_fromJSON?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        populateEventInfoVCLabels()
     }
     
     @IBOutlet weak var map_eventMapView: MKMapView!
@@ -23,7 +24,26 @@ class EventInfoViewController: UIViewController {
     @IBOutlet weak var lbl_eventAddress: UILabel!
     @IBOutlet weak var lbl_eventDate: UILabel!
     @IBOutlet weak var lbl_eventDescription: UILabel!
+    
     @IBAction func btn_getTickets(_ sender: Any) {
+        guard let event = selectedEvent else { return }
+        
+        // ticketmaster search for the event name
+        let searchQuery = event.name
+        let encodedQuery = searchQuery.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let searchURL = "https://www.ticketmaster.com/search?q=\(encodedQuery)"
+        
+        if let url = URL(string: searchURL) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    func populateEventInfoVCLabels() {
+        guard let event = selectedEvent else { return }
+        lbl_eventTitle.text = event.name
+        lbl_eventAddress.text = event.address
+        lbl_eventDate.text = event.date_START
+        lbl_eventDescription.text = event.event_description
     }
     
 }
